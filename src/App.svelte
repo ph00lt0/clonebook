6ba103b8a693289ceb74a4e57327b5f9571cc690
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-04-30 11:17:29 +0200

    add friendlist with data and status

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,18 @@
 <script>
 	import Nav from "./Nav.svelte"
-	import Right from "./Right.svelte"
+	import FriendsList from "./FriendsList.svelte"
+	import Posts from "./Posts.svelte"
+	import {me} from "./data.js"
+
+	setTimeout( ()=> {$me.friends[0].status = 1}, 3000);
+	setTimeout( ()=> {$me.friends[0].status = 0}, 10000);
 </script>
 
 <Nav />
 <main>
-
+<Posts/>
 </main>
-<Right />
+<FriendsList />
 
 
 <style>
---
+++ src/FriendThumbnail.svelte
@@ -0,0 +1,36 @@
+<script>
+    import {me} from "./data.js"
+    export let id;
+    let name = $me.friends.find(friend => friend.id == id).name;
+    let lastName = $me.friends.find(friend => friend.id == id).lastName;
+    $: status = $me.friends.find(friend => friend.id == id).status;
+
+    const showChatWindow = () => {
+        console.log("showing chat window")
+    }
+</script>
+
+
+    <div class="friend {status === 1 ? 'active' : ''}" on:click="{showChatWindow}">
+        :) {name} {lastName}
+    </div>
+
+
+<style>
+    .friend {
+        display: grid;
+        justify-content: center;
+        text-align: center;
+        overflow: hidden;
+        align-content: center;
+        cursor: pointer;
+        width: 2rem;
+        height: 2rem;
+        border: 3px solid black;
+        border-radius: 50%;
+    }
+
+    .friend.active {
+        border-color: seagreen;
+    }
+</style>
