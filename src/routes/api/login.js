c22b42bcdb44c8aa24343a24f5612aa6ded47c25
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-05-29 00:58:32 +0200

    login stuck on mongo query middleware completed

--- src/routes/api/login.js
+++ src/routes/api/login.js
@@ -4,24 +4,20 @@ require('dotenv').config();
 
 export function post(req, res, next) {
     if (!req.body) {
-        res.status(400);
-        return res.err (JSON.stringify({"message": "Bad request"}));
+        return res.status(400).end(JSON.stringify({"message": "Bad request"}));
     }
     try {
         User.findOne( { "username": req.body['username'] }, function(err, result) {
             if(err){
-                res.status(401);
-                return res.end(JSON.stringify({"message": "Incorrect credentials"}));
+                return res.status(401).end(JSON.stringify({"message": "Incorrect credentials"}));
             }
             if(req.body['password'] !== result.password) {
-                res.status(401);
-                return res.end(JSON.stringify({"message": "Incorrect credentials"}));
+                return res.status(401).end(JSON.stringify({"message": "Incorrect credentials"}));
             }
-            const token = jwt.sign({id: result._id, username: result.username},
-                process.env.JWT_SIGNATURE);
+            const token = jwt.sign({id: result._id, username: result.username}, process.env.JWT_SIGNATURE);
             return res.send({'token':token})
         });
     } catch (err) {
-        console.log("Incorrect credentials")
+        return res.status(401).end(JSON.stringify({"message": "Cannot connect to database"}));
     }
 }
