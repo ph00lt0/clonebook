f448f2a4b88c274d81ede36bf9574b6145f4d4fa
Marijn Pool <15004290+ph00lt0@users.noreply.github.com>
2020-06-24 11:10:43 +0200

    add styling to me page

--- src/routes/profile/me.svelte
+++ src/routes/profile/me.svelte
@@ -29,14 +29,41 @@
 </script>
 
 <style>
+    section {
+        padding: 1em;
+        -webkit-box-shadow: 14px 14px 31px 21px rgba(237,237,237,1);
+        -moz-box-shadow: 14px 14px 31px 21px rgba(237,237,237,1);
+        box-shadow: 14px 14px 31px 21px rgba(237,237,237,1);
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        border-radius: 20px;
+    }
 
+    input {
+        height: 2em;
+        font-size: 1em;
+        padding: 0.5em;
+        background: #F6F6F6;
+        border: none;
+        border-radius: 20px;
+        display: block;
+        margin-bottom: 1em;
+        width: 20em;
+        -webkit-box-shadow: 14px 14px 31px 21px rgba(237, 237, 237, 1);
+        -moz-box-shadow: 14px 14px 31px 21px rgba(237, 237, 237, 1);
+        box-shadow: 14px 14px 31px 21px rgba(237, 237, 237, 1);
+    }
+
+    p, h2 {
+        grid-column: 1 / -1;
+    }
 </style>
 
 <svelte:head>
 	<title>Profile of {$user.firstName}</title>
 </svelte:head>
 
-
+<section>
 <h2>Update your information</h2>
 <p>Form automatically saves your input</p>
 <input type="text" value={$user.firstName} on:input={e=> updateProfile('firstName', e.target.value)}/>
@@ -44,3 +71,4 @@
 <input type="email" value={$user.email} on:input={e=> updateProfile('email', e.target.value)}/>
 <input type="text" value={$user.username}  on:input={e=> updateProfile('username', e.target.value)}/>
 <input type="password" value="**********"  on:input={e=> updateProfile('password', e.target.value)}/>
+</section>
